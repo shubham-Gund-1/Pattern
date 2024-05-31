80132cebcd93f1f96a311e9cea086e6a21bab824
shubham-Gund-1 <shubhamgund3480@gmail.com>
2024-05-31 10:38:04 +0530

    Pattern

---
+++ pattern1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pattern1{
+    public static void main(String args[]){
+        int row,col,n;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a number :");
+        n=input.nextInt();
+        for(row=1;row<=n;row++){
+            for(col=1;col<=row;col++){
+                System.out.print(" "+col);
+            }
+            System.out.println();
+        }
+    }
+}
