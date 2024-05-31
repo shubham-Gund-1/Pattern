f716d406644e12f7ccdc3393250b7674b1003617
shubham-Gund-1 <shubhamgund3480@gmail.com>
2024-05-31 17:59:10 +0530

    pattern

--- pattern1.java
+++ pattern1.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class pattern1{
+public class Pattern1{
     public static void main(String args[]){
         int row,col,n;
         Scanner input=new Scanner(System.in);
